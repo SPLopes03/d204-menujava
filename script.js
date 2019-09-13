5021eff50cd76c133a0d3d9eb825ecd75ca4d971
AndreLp <andrelopes.spdia@gmail.com>
2019-09-13 11:57:45 -0300

    estilizar menu e nome

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 let menuHamburguer = document.querySelector(".menu-hamburguer");
 let nav = document.querySelector("nav");
+let input = document.querySelector("input");
+let botao = document.querySelector("button");
+let h2 = document.querySelector("h2");
 
 function mostrarMenu() {
     // nav.style.display = "block";
@@ -9,9 +12,17 @@ function mostrarMenu() {
     // }else{
     //     nav.classList.add("visivel");
     // }
-    
+
     //JEITO 3
     nav.classList.toggle("visivel");
+    menuHamburguer.classList.toggle("ativo");
+}
+
+function pegarNome() {
+    let nome = input.value;
+    h2.innerHTML = `Seja Bem-Vindo(a) ${nome}`;
+    input.value = "";
 }
 
 menuHamburguer.onclick = mostrarMenu;
+botao.onclick = pegarNome;
